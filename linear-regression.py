a259d2a547a33dd76c60ab4ae6df8d31c23b8bae
uhassam <umarhassam224@gmail.com>
2024-01-03 08:03:49 -0600

    Add definitions to file

---
+++ linear-regression.py
@@ -0,0 +1,17 @@
+class LinearRegression():
+    def __init__(self) -> None:
+        pass
+
+    def yhat(self, x, weights):
+        pass
+
+    def loss(self, yhat, y):
+        pass
+
+    def gradient_descent(self, weights, X, y, yhat):
+        pass
+
+    def main():
+        pass
+
+    
