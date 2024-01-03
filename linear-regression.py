1891801fbdebbabaaba132913f2cf6eab8d69bb2
uhassam <umarhassam224@gmail.com>
2024-01-03 08:37:23 -0600

    Add procedures to init, yhat, and loss

--- linear-regression.py
+++ linear-regression.py
@@ -1,14 +1,21 @@
+import numpy as np
+
 class LinearRegression():
     #initialize
     def __init__(self) -> None:
-        pass
-
-    def yhat(self, x, weights):
-        pass
-
+        self.learning_rate = 0.01
+        self.iterations = 15000
+    
+    #predicted y value
+    def yhat(self, X, weights):
+        return np.dot(weights.T, X)
+    
+    #loss function
     def loss(self, yhat, y):
-        pass
+        lossret = 1/self.m * np.sum(np.power(yhat - y, 2))
+        return lossret
 
+    #gradient descent function
     def gradient_descent(self, weights, X, y, yhat):
         pass
 
